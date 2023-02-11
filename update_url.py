db88b2c01fc2b95e8301d3d7590e4cd0c0caae97
钟孟捷 <718335940@qq.com>
2023-02-12 04:40:28 +0800

    update_url

---
+++ update_url.py
@@ -0,0 +1,17 @@
+from subprocess import getoutput
+
+import requests
+
+alias = 'config'
+url = f"https://cdn.jsdelivr.net/gh/zsokami/ACL4SSR@{getoutput('git rev-parse HEAD')}/ACL4SSR_Online_Full_Mannix.ini"
+
+session = requests.Session()
+session.headers['Authorization'] = 'Bearer wMZJfKSns5lLIZ7if32owHe9w06EVAV6ZjbnCoeFs65PNN95lrwDxnKSGAMV'
+api = 'https://goo.gs/api/v1/links'
+items = session.get(api, params={'search': alias, 'by': 'alias'}).json()['data']
+for item in items:
+    if item['alias'] == alias:
+        print(session.put(f"{api}/{item['id']}", data={'url': url}).ok)
+        break
+else:
+    print(session.post(api, data={'url': url, 'alias': alias}).ok)
